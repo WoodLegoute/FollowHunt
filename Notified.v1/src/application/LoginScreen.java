8453c10a9c4a0e4661c731afa46c2e07fbdffd6a
legoute21 <legoute21@gmail.com>
2017-11-08 15:41:08 -0500

    Created Java Project in Eclipse

---
+++ Notified.v1/src/application/LoginScreen.java
@@ -0,0 +1,26 @@
+package application;
+
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginScreen {
+
+    @FXML
+    private JFXTextField usernameField;
+
+    @FXML
+    private JFXTextField passwordField;
+
+    @FXML
+    void f8f5f5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void e8e5e5(ActionEvent event) {
+
+    }
+
+}
