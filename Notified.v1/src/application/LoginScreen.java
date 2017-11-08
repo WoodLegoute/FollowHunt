f85e1ddfb2fec86da14c04da78fdd50078200936
legoute21 <legoute21@gmail.com>
2017-11-08 16:59:57 -0500

    Added Exit Button And fxid

--- Notified.v1/src/application/LoginScreen.java
+++ Notified.v1/src/application/LoginScreen.java
@@ -1,17 +1,26 @@
 package application;
 
 import com.jfoenix.controls.JFXTextField;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class LoginScreen {
+public class LoginScreenController {
 
     @FXML
     private JFXTextField usernameField;
 
     @FXML
+    private Label testPrompt;
+
+    @FXML
     private JFXTextField passwordField;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button close;
 
     @FXML
     void f8f5f5(ActionEvent event) {
@@ -19,8 +28,19 @@ public class LoginScreen {
     }
 
     @FXML
+    void loginButton(ActionEvent event) {
+
+    }
+
+    @FXML
     void e8e5e5(ActionEvent event) {
 
+    }
+
+    @FXML
+    void closeButton(ActionEvent event) {
+    		
+    	testPrompt.setText("This will close the app");
     }
 
 }
